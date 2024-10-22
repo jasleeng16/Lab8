4ee32367bc83d502f5f4ef07fa50e5fa7059a71f
“jasleeng16” <gilljasleen234@gmail.com>
2024-10-22 17:53:56 -0600

    deleteCity functionality commit

--- app/src/main/java/com/example/lab8/CustomList.java
+++ app/src/main/java/com/example/lab8/CustomList.java
@@ -77,7 +77,12 @@ public class CustomList extends ArrayAdapter<City> {
      *  This is a city to delete.
      */
     public void deleteCity(City city){
-
+        if (cities.contains(city)) {
+            cities.remove(city);
+        }
+        else{
+            throw new IllegalArgumentException("City is not present in the list.");
+        }
     }
 
 }
